c09830a429e0e5ac9923d323b15bc9a0da448613
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-27 20:01:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,10 +8,12 @@ end
 
 post '/piglatinize' do
 
-  ring = PigLatinizer.new
-  @string = ring.pig(params[:user_phrase])
-  erb :piglatinize
+  post '/piglatinize' do
+     user_word = params["user_phrase"]
+     word = PigLatinizer.new
+     @pig_latinized_word = word.to_pig_latin(user_word)
 
+     erb :pit_latin
 end
 
 end
